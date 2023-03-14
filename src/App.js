da4c0fb63e0908b57fb75be2b59873c344e95853
Sarang Lohe <saranglohe1999@gmail.com>
2023-03-14 22:53:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./component/Home/Home";
+import { Routes } from "react-router-dom";
+import Product from "./component/Product/Product";
+import SingleProduct from "./component/SingleProduct/SingleProduct";
+import { Route } from "react-router-dom";
+import About from "./component/About/About";
+import Contact from "./component/contact/Contact";
+import Footer from "./component/Footer/Footer";
+import Header from "./component/Header/Header";
+import Createaccount from "./Createaccount";
+import CheckOut from "./component/Checkout/CheckOut";
+import Cart from "./component/Cart/Cart";
+import Homee from "./Homee";
+
+const ErrorComponent = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItem: "center",
+        }}
+      >
+        <img
+          src="./assets/error.jpg"
+          alt="error"
+          height={"500px"}
+          width={"700px"}
+        />
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     
+      {/* <Header/> */}
+      <Routes>
+        <Route path="/" element={<Homee/>}/>
+        <Route path="/header" element={<Header/>}/>        
+        <Route path="/products" element={<Product/>}/>
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/checkout" element={<CheckOut />} />
+        <Route path="/createaccount" element={<Createaccount />} />
+        <Route path="*" element={<ErrorComponent/>}/>
+        
+      </Routes>
+      
+
+    </>
   );
 }
 
